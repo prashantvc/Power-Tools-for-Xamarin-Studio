10178213610075131f5dcef8667671f0c1a16d5f
Prashant C <pvc@outlook.com>
2016-05-17 21:32:40 +0530

    Initial commit

---
+++ PowerTools/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"PowerTools",
+	Namespace = "PowerTools",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("PowerTools")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("PowerTools")]
+[assembly: AddinAuthor("prashant")]
+
